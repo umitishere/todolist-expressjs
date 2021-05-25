1beb3f127d889c0199d597ba4bfc96f83f41f948
umitishere <umitsancakli98@gmail.com>
2021-05-25 11:39:30 +0300

    Show the name of current day

--- app.js
+++ app.js
@@ -2,24 +2,44 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs"); 
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    
+
     let today = new Date();
     let currentDay = today.getDay();
     let day = "";
 
-    if (currentDay === 6 || currentDay === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error: current day is " + currentDay);
     }
 
     // It has to be in the views folder and has to have .ejs extension (embedded js)
-    res.render("list", {kindOfDay: day});
+    res.render("list", { kindOfDay: day });
 
 });
 
